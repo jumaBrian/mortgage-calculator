ea4f3d8c4d2beb140e2ddfe87cdb1c4c3b76357f
jumaBrian <njengabrianj@gmail.com>
2022-04-27 22:17:12 +0300

    Declared equation variables

--- src/com/brayocodes/Main.java
+++ src/com/brayocodes/Main.java
@@ -1,11 +1,27 @@
 package com.brayocodes;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Principal: ");
+//        int principal = scanner.nextInt();
+
+        System.out.println("Annual Interest Rate: ");
+//        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest/PERCENT/MONTHS_IN_YEAR;
+
+        System.out.println("Period (YEARS): ");
+//        byte years = scanner.nextByte();
+        int numberOfPayments = years*MONTHS_IN_YEAR;
+
 
     }
 }
